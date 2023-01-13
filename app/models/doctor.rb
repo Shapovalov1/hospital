6c8a7789e6b9f368672d8d64e1c9a3934d128270
Shapovalov1 <zatilok1701@gmail.com>
2023-01-13 00:28:07 +0000

    change styles

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < User
   has_many :appointments
   has_many :patients, through: :appointments
-  belongs_to :category, required: false
+  belongs_to :category, required: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :phone_number, uniqueness: true, phone: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def email_required?
     false
